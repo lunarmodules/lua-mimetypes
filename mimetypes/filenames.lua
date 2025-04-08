cedb0ee87a55e8e759d2b73462ecfc255458ee90
Thijs Schreijer <thijs@thijsschreijer.nl>
2025-04-08 23:57:21 +0200

    chore(update): generate list from Apache httpd

---
+++ mimetypes/filenames.lua
@@ -0,0 +1,8 @@
+-- This contains filename overrides for certain files, like README files.
+
+return {
+    COPYING             = 'text/plain',
+    LICENSE             = 'text/plain',
+    Makefile            = 'text/x-makefile',
+    README              = 'text/plain',
+}
